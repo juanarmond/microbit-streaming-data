d31e71fa324ac78f008c06681504eb5cbfd1cf58
Juan Armond <juan_armond@hotmail.com>
2021-10-31 12:00:22 +0000

    Fix Glue bug

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-from data_lake.stack import DataLakeStack
-from kinesis.stack import KinesisStack
+from microbit.data_lake.stack import DataLakeStack
+from microbit.kinesis.stack import KinesisStack
+from microbit.glue_catalog.stack import GlueCatalogStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
 kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
+glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
 app.synth()
