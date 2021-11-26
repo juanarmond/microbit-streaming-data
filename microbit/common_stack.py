af3cc8cb1c2e143432bdecfc0b10036b6179a5a5
Juan Armond <juan_armond@hotmail.com>
2021-11-26 13:18:13 +0000

    Add Redshift

--- microbit/common_stack.py
+++ microbit/common_stack.py
@@ -1,6 +1,6 @@
 from aws_cdk import core
-from aws_cdk import aws_rds as rds, aws_ec2 as ec2
-from data_platform import active_environment
+from aws_cdk import aws_ec2 as ec2
+from microbit import active_environment
 
 
 class CommonStack(core.Stack):
@@ -27,38 +27,3 @@ class CommonStack(core.Stack):
                 peer=ec2.Peer.ipv4(subnet.ipv4_cidr_block),
                 connection=ec2.Port.tcp(5432),
             )
-
-        self.orders_rds_parameter_group = rds.ParameterGroup(
-            self,
-            f"orders-{self.deploy_env.value}-rds-parameter-group",
-            description="Parameter group to allow CDC from RDS using DMS.",
-            engine=rds.DatabaseInstanceEngine.postgres(
-                version=rds.PostgresEngineVersion.VER_12_4
-            ),
-            parameters={"rds.logical_replication": "1", "wal_sender_timeout": "0"},
-        )
-
-        self.orders_rds = rds.DatabaseInstance(
-            self,
-            f"orders-{self.deploy_env.value}-rds",
-            engine=rds.DatabaseInstanceEngine.postgres(
-                version=rds.PostgresEngineVersion.VER_12_4
-            ),
-            database_name="orders",
-            instance_type=ec2.InstanceType("t3.micro"),
-            vpc=self.custom_vpc,
-            instance_identifier=f"rds-{self.deploy_env.value}-orders-db",
-            port=5432,
-            vpc_placement=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
-            subnet_group=rds.SubnetGroup(
-                self,
-                f"rds-{self.deploy_env.value}-subnet",
-                description="place RDS on public subnet",
-                vpc=self.custom_vpc,
-                vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
-            ),
-            parameter_group=self.orders_rds_parameter_group,
-            security_groups=[self.orders_rds_sg],
-            removal_policy=core.RemovalPolicy.DESTROY,
-            **kwargs,
-        )
