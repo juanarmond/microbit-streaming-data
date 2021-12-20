ab16d0d9429a3c5b597f9420c290a39c0d95f27e
Juan Armond <juan_armond@hotmail.com>
2021-12-20 17:30:22 +0000

    Added AWS Lambda function

--- microbit/lambda_functions/stack.py
+++ microbit/lambda_functions/stack.py
@@ -2,6 +2,50 @@ from aws_cdk import core as cdk, aws_lambda as _lambda, aws_iam as iam
 from microbit.data_lake.base import BaseDataLakeBucket
 from microbit import active_environment
 
+
+class LambdaRole(iam.Role):
+    def __init__(
+        self,
+        scope: cdk.Construct,
+        data_lake_processed: BaseDataLakeBucket,
+        **kwargs,
+    ) -> None:
+        self.deploy_env = active_environment
+        self.data_lake_processed = data_lake_processed
+
+        super().__init__(
+            scope,
+            id=f"iam-{self.deploy_env.value}-lambda-functions-role",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            description="Role to allow Lambda to save data to data lake processed",
+        )
+
+    def add_policy(self):
+        policy = iam.Policy(
+            self,
+            id=f"iam-{self.deploy_env.value}-data-lake-processed-lambda-functions-policy",
+            policy_name=f"iam-{self.deploy_env.value}-data-lake-processed-lambda-functions-policy",
+            statements=[
+                iam.PolicyStatement(
+                    actions=[
+                        "s3:AbortMultipartUpload",
+                        "s3:GetBucketLocation",
+                        "s3:GetObject",
+                        "s3:ListBucket",
+                        "s3:ListBucketMultipartUploads",
+                        "s3:PutObject",
+                    ],
+                    resources=[
+                        self.data_lake_processed_bucket.bucket_arn,
+                        f"{self.data_lake_processed_bucket.bucket_arn}/*",
+                    ],
+                )
+            ],
+        )
+        self.attach_inline_policy(policy)
+
+
+
 class LambdaFunctionsStack(cdk.Stack):
     def __init__(
         self,
@@ -38,4 +82,12 @@ class LambdaFunctionsStack(cdk.Stack):
                     f"{self.processed_data_lake_bucket.bucket_arn}/*",
                 ],
             )
+        )
+
+    @property
+    def lambda_functions_role(self):
+        return LambdaRole(
+            self,
+            deploy_env=self.deploy_env,
+            data_lake_raw_bucket=self.data_lake_processed_bucket,
         )
