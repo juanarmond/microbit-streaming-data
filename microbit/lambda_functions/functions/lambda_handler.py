eff6918cf76bc99c66f2ec00f2aebc3e5b120be0
Juan Armond <juan_armond@hotmail.com>
2021-12-21 14:54:27 +0000

    Added AWS Lambda function

--- microbit/lambda_functions/functions/lambda_handler.py
+++ microbit/lambda_functions/functions/lambda_handler.py
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
         for content in response.get("Contents", []):
             # print(content.get("Key"))
             key=content.get("Key")
-            obj = client.get_object(bucket_name=bucket, key=key)
+            obj = client.Object(bucket_name=bucket, key=key)
             with gzip.GzipFile(fileobj=obj.get()) as gzipfile:
                 content = gzipfile.read()
                 print(content)
