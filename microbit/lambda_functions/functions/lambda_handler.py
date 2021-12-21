cbd8ff9cb712f0d9dc8674d226adb85f8418922e
Juan Armond <juan_armond@hotmail.com>
2021-12-21 12:20:17 +0000

    Added AWS Lambda function

--- microbit/lambda_functions/functions/lambda_handler.py
+++ microbit/lambda_functions/functions/lambda_handler.py
@@ -2,7 +2,7 @@ import boto3
 
 print("Loading function")
 
-s3 = boto3.client("s3-microbit-data-develop-data-lake-raw")
+client = boto3.client('data-lake-raw')
 
 
 def lambda_handler(event, context):
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     # Get the object from the event and show its content type
     # s3: // s3 - microbit - data - develop - data - lake - raw / atomic_events / date = 2021 - 11 - 26 /
     try:
-        response = s3.list_objects(Bucket="s3-microbit-data-develop-data-lake-raw", Prefix="atomic_events")
+        response = client.list_objects(Bucket="s3-microbit-data-develop-data-lake-raw", Prefix="atomic_events")
         for content in response.get("Contents", []):
             print(content.get("Key"))
         # return response["ContentType"]
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
         print(e)
         print(
             "Error getting objects from bucket {}. Make sure they exist and your bucket is in the same region as this function.".format(
-                s3
+                client
             )
         )
         raise e
