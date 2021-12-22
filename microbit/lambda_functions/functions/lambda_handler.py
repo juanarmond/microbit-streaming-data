64014abdb8e2bdb47659d79a56d2188d3b240dd0
Juan Armond <juan_armond@hotmail.com>
2021-12-22 11:33:05 +0000

    Added AWS Lambda function

--- microbit/lambda_functions/functions/lambda_handler.py
+++ microbit/lambda_functions/functions/lambda_handler.py
@@ -11,28 +11,16 @@ def lambda_handler(event, context):
     # Get the object from the event and show its content type
     # s3: // s3 - microbit - data - develop - data - lake - raw / atomic_events / date = 2021 - 11 - 26 /
     bucket = "s3-microbit-data-develop-data-lake-raw"
-    try:
-        response = client.list_objects(Bucket=bucket, Prefix="atomic_events")
-        for content in response.get("Contents", []):
-            # print(content.get("Key"))
-            key = content.get("Key")
-            obj = client.get_object(Bucket=bucket, Key=key)
-            gzip_content = obj['Body'].read()
-            with gzip.GzipFile(fileobj=BytesIO(gzip_content), mode='rb') as gzipfile:
-                content = gzipfile.read()
-                for line in content.decode().split("\n"):
-                    # print('l1: ',line)
-                    dic = json.loads(line)
-                    print('dic',dic['key'])
+    response = client.list_objects(Bucket=bucket, Prefix="atomic_events")
+    for content in response.get("Contents", []):
+        # print(content.get("Key"))
+        key = content.get("Key")
+        obj = client.get_object(Bucket=bucket, Key=key)
+        gzip_content = obj['Body'].read()
+        with gzip.GzipFile(fileobj=BytesIO(gzip_content), mode='rb') as gzipfile:
+            content = gzipfile.read()
+            for line in content.decode().split("\n"):
+                # print('l1: ',line)
+                dic = json.loads(line)
+                print('dic',dic['key'])
         # return response["ContentType"]
-    except Exception as e:
-        print(e)
-        print('line', [line])
-        # print(content.decode())
-
-        # print(
-        #     "Error getting objects from bucket {}. Make sure they exist and your bucket is in the same region as this function.".format(
-        #         client
-        #     )
-        # )
-        raise e
