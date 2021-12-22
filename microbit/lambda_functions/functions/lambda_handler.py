83bd7c1bc14572f0a47485a3ae37d8ef96e7104c
Juan Armond <juan_armond@hotmail.com>
2021-12-22 11:22:26 +0000

    Added AWS Lambda function

--- microbit/lambda_functions/functions/lambda_handler.py
+++ microbit/lambda_functions/functions/lambda_handler.py
@@ -21,9 +21,9 @@ def lambda_handler(event, context):
             with gzip.GzipFile(fileobj=BytesIO(gzip_content), mode='rb') as gzipfile:
                 content = gzipfile.read()
                 for line in content.decode().split("\n"):
-                    print('l1: ',line)
-                    dic = json.loads(line.rstrip())
-                    print('dic',dic)
+                    # print('l1: ',line)
+                    dic = json.loads(line)
+                    print('dic',dic['key'])
         # return response["ContentType"]
     except Exception as e:
         print(e)
