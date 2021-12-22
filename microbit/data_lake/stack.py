2926bf89b1b2ed3d6c629b4a668c5bd63f9eb3d3
Juan Armond <juan_armond@hotmail.com>
2021-12-22 14:41:38 +0000

    Added AWS Lambda function

--- microbit/data_lake/stack.py
+++ microbit/data_lake/stack.py
@@ -2,11 +2,10 @@ from microbit.data_lake.base import BaseDataLakeBucket, DataLakeLayer
 from aws_cdk import core
 from aws_cdk import (
     aws_s3 as s3,
-    aws_s3_notifications as s3n
+    aws_s3_notifications
 )
 
 from microbit import active_environment
-from microbit.lambda_functions.stack import LambdaFunctionsStack
 
 
 class DataLakeStack(core.Stack):
@@ -15,11 +14,11 @@ class DataLakeStack(core.Stack):
         super().__init__(scope, id=f"{self.deploy_env.value}-data-lake-stack", **kwargs)
 
         # Data Lake Raw
-        self.data_lake_raw = BaseDataLakeBucket(
+        self.data_lake_raw_bucket = BaseDataLakeBucket(
             self, deploy_env=self.deploy_env, layer=DataLakeLayer.RAW
         )
 
-        self.data_lake_raw.add_lifecycle_rule(
+        self.data_lake_raw_bucket.add_lifecycle_rule(
             transitions=[
                 s3.Transition(
                     storage_class=s3.StorageClass.INTELLIGENT_TIERING,
@@ -34,12 +33,6 @@ class DataLakeStack(core.Stack):
         )
 
         # Data Lake Processed
-        self.data_lake_processed = BaseDataLakeBucket(
+        self.data_lake_processed_bucket = BaseDataLakeBucket(
             self, deploy_env=self.deploy_env, layer=DataLakeLayer.PROCESSED
         )
-
-        # # create s3 notification for lambda function
-        # notification = s3n.LambdaDestination(LambdaFunctionsStack.fn)
-        #
-        # # assign notification for the s3 event type (ex: OBJECT_CREATED)
-        # self.data_lake_raw.add_event_notification(s3.EventType.OBJECT_CREATED, notification)
