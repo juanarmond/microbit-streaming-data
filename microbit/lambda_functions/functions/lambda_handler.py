7924bdf9661e4cdf99bebf55b6f92dc8ee3c2ec6
Juan Armond <juan_armond@hotmail.com>
2022-01-05 11:30:18 +0000

    Added AWS Lambda function

--- microbit/lambda_functions/functions/lambda_handler.py
+++ microbit/lambda_functions/functions/lambda_handler.py
@@ -7,18 +7,19 @@ client = boto3.client("s3")
 
 def lambda_handler(event, context):
     # Get the object from the event and show its content type
-    list = ["carSN", "a.x", "a.y", "a.z", "tembo", "temp", "dist", "light", "sound", "compass", "mag.x", "mag.y",
-            "mag.z", "mag.str", "left", "right", "mv", "seconds"]
-    bucket = "s3-microbit-data-develop-data-lake-raw"
+    data_list = ["carSN", "a.x", "a.y", "a.z", "tembo", "temp", "dist", "light", "sound", "compass", "mag.x", "mag.y",
+                 "mag.z", "mag.str", "left", "right", "mv", "seconds"]
+    data_lake_raw = "s3-microbit-data-develop-data-lake-raw"
+    data_lake_processed = "s3-microbit-data-develop-data-lake-processed"
     data = ""
     try:
-        response = client.list_objects(Bucket=bucket, Prefix="atomic_events")
+        response = client.list_objects(Bucket=data_lake_raw, Prefix="atomic_events")
         print("Total Responses: ", len(response.get("Contents", [])))
         for content in response.get("Contents", []):
             # print(content.get("Key"))
             key = content.get("Key")
             print(key)
-            obj = client.get_object(Bucket=bucket, Key=key)
+            obj = client.get_object(Bucket=data_lake_raw, Key=key)
             gzip_content = obj["Body"].read()
             with gzip.GzipFile(fileobj=BytesIO(gzip_content), mode="rb") as gzipfile:
                 content = gzipfile.read()
@@ -28,13 +29,17 @@ def lambda_handler(event, context):
                         if "k" not in line:
                             line = line.replace('""', '"k"')
                         dic = json.loads(line)
-                        if dic["k"] in list:
+                        if dic["k"] in data_list:
                             data = f"{data}{json.dumps(dic)}\n"
                         else:
                             print("NOT IN THE LIST", dic["k"])
                 if data:
-                    # with gzip.GzipFile(fileobj=data, mode="w") as new_gzipfile:
                     print(data)
+                    with gzip.GzipFile(fileobj=data, mode="w") as new_gzipfile:
+                        client.Bucket(data_lake_processed).put_object(
+                            Key="atomic_events/date=!{timestamp:yyyy}-!{timestamp:MM}-!{timestamp:dd}/",
+                            Body=new_gzipfile)
+
                 else:
                     print('NO DATA')
             print("-" * 100)
